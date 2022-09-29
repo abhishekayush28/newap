009452c8bc74754ae376b186333fb0513b16eec5
Ayush Abhishek <65723731+abhishekayush28@users.noreply.github.com>
2022-09-29 06:58:04 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+	{
+		email: { type: String, required: true, unique: true },
+		passwordh: { type: String, required: true }
+	},
+	{ collection: 'users' }
+)
+
+const model = mongoose.model('UserSchema', UserSchema)
+
+module.exports = model
---
+++ server.js
@@ -0,0 +1,147 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const Users = require('./models/user')
+const Admin = require('./models/admin')
+const bodyParser = require("body-parser")
+const bcrypt = require('bcrypt')
+const articleRouter = require('./routes/articles')
+
+
+const methodOverride = require('method-override')
+const { json } = require('express')
+const article = require('./models/article')
+const app = express()
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
+mongoose.connect('mongodb://localhost:27017/blog1', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use(express.json());
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', async (req, res) => {
+  res.render('home')
+  
+})
+
+app.get('/login', async (req, res) => {
+ res.render('user/login')
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const email = req.body.email;
+    const userpassword = req.body.password;
+    const useremail = await Users.findOne({ email: email });
+    
+    const ismatch = await bcrypt.compare(userpassword,useremail.passwordh); 
+    if (!ismatch) {
+      res.send("Incorect Password");
+    }
+    else {
+      const articles = await Article.find().sort({ createdAt: 'desc' })
+      res.status(201).render('landpage', { articles: articles });
+    }
+
+  } catch (error) {
+    res.status(400).send("Invalid Email")
+  }
+
+});
+
+
+app.get('/register', async (req, res) => {
+  res.render('user/register')
+})
+
+app.post('/register', async (req, res) => {
+
+  const { email, password, password2 } = req.body;
+  
+  const passwordh = bcrypt.hashSync(password, salt);
+ 
+  try {
+    let response = await Users.create({
+      email,passwordh
+    })
+    console.log("sucesssss", response);
+    res.render('user/login')
+  } catch (error) {
+   if (error.code === 11000) {
+     return res, json({ status: "error", error: "This emailid already exists." })
+    }
+    throw error
+  }
+});
+
+
+app.get('/adminlogin', async (req, res) => {
+  res.render('admin/adminlogin')
+})
+
+app.post('/adminlogin', async (req, res) => {
+  try {
+    const email = req.body.email;
+    const userpassword = req.body.password;
+
+    const useremail = await Admin.findOne({ email: email });
+
+    const ismatch = await bcrypt.compare(userpassword,useremail.passwordh);
+    if (ismatch) {
+      const articles = await Article.find().sort({ createdAt: 'desc' })
+      res.render('articles/index', { articles: articles })
+    }
+    else {
+      res.send("Incorrect password");
+    }
+
+  } catch (error) {
+    res.status(400).send("Invalid Email")
+  }
+});
+
+
+app.get('/adminregister', async (req, res) => {
+  res.render('admin/adminregister')
+})
+
+
+app.post('/adminregister', async (req, res) => {
+
+  const { email, password, password2, key } = req.body;
+  const passwordh = bcrypt.hashSync(password, salt);
+
+  try {
+    if (key === "root") {
+      let response = await Admin.create({
+        email, passwordh
+      })
+      console.log("sucesssss", response);
+      res.render('admin/adminlogin')
+    }
+    else {
+      return res.json({ status: "error", error: "Enter a valid key." })
+    }
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.json({ status: "error", error: "This emailidalready exists." })
+    }
+    throw error
+  }
+});
+
+app.get('/index', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+});
+
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
